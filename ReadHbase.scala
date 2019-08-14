4077557ebc1f837d77dde951eea019acd867c2f0
st57701 <anatolijmironov@gmail.com>
2019-08-15 00:11:30 +0300

    rows are writing in array and rdd

--- ReadHbase.scala
+++ ReadHbase.scala
@@ -6,6 +6,7 @@ import org.apache.hadoop.hbase.io.ImmutableBytesWritable
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.hadoop.hbase.client._
+import scala.collection.mutable.ArrayBuffer
 
 import org.apache.spark._
 object ReadHbase {
@@ -15,14 +16,18 @@ object ReadHbase {
     hconf.set("hbase.zookeeper.property.clientPort", "2181")
     val conf = new SparkConf().setAppName("HbaseRead").setMaster("local[*]")
     val sc = new SparkContext(conf)
-
+  
     val columnFamily1 = "metadata"
     val columnFamily2 = "payload"
     val tableName = "spark_hbase_task"
     val hTable = new HTable(hconf, tableName)
-     val stringArray = Array("001","002","007")
-     for(line <- 0 to stringArray.length){
-    val g = new Get(Bytes.toBytes(stringArray(line)))
+    val rdd5 = Seq()
+     val stringArr = Array("001","002","007") 
+     var newData = new Array[String](3)
+for(line<- 0 to stringArr.length-1){
+    val g = new Get(Bytes.toBytes(stringArr(line)))
+  
+  
     val result = hTable.get(g)
     val id = result.getValue(Bytes.toBytes("metadata"),Bytes.toBytes("id"))
     val name = result.getValue(Bytes.toBytes(columnFamily2),Bytes.toBytes("name"))
@@ -33,10 +38,20 @@ object ReadHbase {
       
     val df =(id,name,l_name,data,comments)
      println("WRRRRRRRRRRRRRRRRRONG ROWKEY")
-     val df1 = Seq((Bytes.toString(df._1),Bytes.toString(df._2),Bytes.toString(df._3),Bytes.toString(df._4),Bytes.toString(df._5)))
-     val rdd = sc.parallelize(df1)
+    val df1 = (Bytes.toString(df._1),Bytes.toString(df._2),Bytes.toString(df._3),Bytes.toString(df._4),Bytes.toString(df._5))
+     
+     newData(line)+= df1
+     println("Array _ " + newData(line))
+}
+  println(newData.length)
+  println(newData(0))
+  println("Pidarasdasd")
+    val rdd = sc.parallelize(newData)
+    
      rdd.collect().foreach(println)
-     }
+     
+     
+  
    //  val rdd = df1.
     //val value=result.value().
     //println(Bytes.toString(value))

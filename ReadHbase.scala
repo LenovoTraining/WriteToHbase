8f6c8dab9db6aad0aacbdc4afcd7e94cc5d0c9fc
st57701 <anatolijmironov@gmail.com>
2019-08-21 01:46:30 +0300

    DataFrame with out ( ) and with raplace in date and comments

--- ReadHbase.scala
+++ ReadHbase.scala
@@ -7,9 +7,14 @@ import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.hadoop.hbase.client._
 import scala.collection.mutable.ListBuffer
+import scala.collection.mutable._
 import org.apache.spark._
+import org.apache.spark.sql.Row
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.functions._
 object ReadHbase {
- case class Person(name: String, l_name: String, date: String, comments: String, id: String)
+ case class Person(id: String, name: String, l_name: String, date: String, comments: String)
   
   def main(args: Array[String]) {
    
@@ -45,17 +50,30 @@ val sqlContext = new org.apache.spark.sql.SQLContext(sc)
     
      newData(line)+=df
      }
-      val blacklist: Set[Char] = Set('(')
-      val list2 =fruits.map(line => line.filterNot(c => blacklist.contains(c)))
-    val rdd = sc.parallelize(list2)
+  val list = fruits.toList
+    val charsToClean: List[String] = List("\"", "(", ")", "'")
+
+    val filtered : List[String] = list.map(line => charsToClean.foldLeft(line)(_.replace(_, " ")))
+  
+ 
+    //val l1 = List[String](filtered(0),filtered(1),filtered(2))
+   
     
       import sqlContext.implicits._
-      rdd.collect().foreach(println)
       
-      val rdd2 = rdd.map(_.split(",")).map(p =>Person(p(0),p(1),p(2),p(3),p(4)))
+      
+      val rdd2 =filtered.map(_.split(",")).map(p =>Person(p(0),p(1),p(2),p(3),p(4)))
      // rdd2.collect().foreach(println)
       val df=  rdd2.toDF()
+      println("The First DataFrame")
       df.show()
+      println("The Second DataFrame")
+    val df1 = df.withColumn("date", when(col("date") === "", "Date – 01-01-1900").otherwise(col("date")));
+  
+  df1.show()
+  val df2 = df1.withColumn("comments",when(col("comments")==="","String: NA").otherwise(col("comments")));
+      println("The Third DataFrame")                 
+      df2.show()
    //  val rdd = df1.
     //val value=result.value().
     //println(Bytes.toString(value))
